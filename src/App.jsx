8c41b1f3d12cf5dc119632ea18249bd6c51574f3
Maycon Lopes <lopes.maycon@gmail.com>
2023-03-19 16:18:37 -0300

    Converted App to funcional components

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,42 @@
-import { Component } from 'react'
 import './App.css'
 import CardList from './components/card-list/card-list.component'
 import SearchBox from './components/search-box/search-box.component'
+import { useEffect, useState } from 'react'
 
-class App extends Component {
-	constructor() {
-		super()
+function App() {
+	const [cats, setCats] = useState([])
+	const [searchField, setSearchField] = useState('')
 
-		this.state = {
-			cats: [],
-			searchField: '',
-		}
-	}
-
-	componentDidMount() {
+	useEffect(() => {
 		const apiUrl = 'http://jsonplaceholder.typicode.com/users'
 		fetch(apiUrl)
 			.then((response) => response.json())
-			.then((cats) =>
-				this.setState(
-					() => {
-						return { cats: cats }
-					},
-					() => {
-						console.log(this.state)
-					}
-				)
-			)
+			.then(cats => setCats(cats) )
+				
+	}, []) 
+
+
+	function onSearchChange(event) {
+		const search = event.target.value.toLowerCase()
+		setSearchField(search)
 	}
 
-	onSearchChange = (event) => {
-		const searchField = event.target.value.toLowerCase()
-		this.setState(() => {
-			return { searchField }
-		})
-	}
+	const filteredCats = cats.filter((cat) => {
+		return cat.name.toLowerCase().includes(searchField)
+	})
 
-	render() {
-		const { cats, searchField } = this.state
-		const { onSearchChange } = this
-
-		const filteredCats = cats.filter((cat) => {
-			return cat.name.toLowerCase().includes(searchField)
-		})
-
-		return (
-			<div className="App">
-				<h1 className="app-title"> Cute Cats </h1>
-				<SearchBox
-					className="cats-search-box"
-					onChangeHandler={onSearchChange}
-					placeholder="search cats"
-				/>
-
-				<CardList cats={filteredCats} />
-			</div>
-		)
-	}
+	return (
+		<div className="App">
+			<h1 className="app-title"> Cute Cats </h1>
+			<SearchBox
+				className="cats-search-box"
+				onChangeHandler={onSearchChange}
+				placeholder="search cats"
+			/>
+ 			<CardList cats={filteredCats} />
+		</div>
+	)
 }
+
 
 export default App
